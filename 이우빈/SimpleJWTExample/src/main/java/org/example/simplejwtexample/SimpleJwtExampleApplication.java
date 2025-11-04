1110aac99a7b750df25fc17bce2b5a18309b7eb8
0xwb7 <woobin200111@gmail.com>
2025-11-04 09:45:14 +0900

    ci: build, application.yml 설정

---
+++ 이우빈/SimpleJWTExample/src/main/java/org/example/simplejwtexample/SimpleJwtExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.simplejwtexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJwtExampleApplication.class, args);
+	}
+
+}

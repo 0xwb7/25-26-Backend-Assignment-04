86e7fc5c08f35dde6ac448be2c538b4e5db06814
0xwb7 <woobin200111@gmail.com>
2025-11-06 21:51:37 +0900

    refactor: 불필요 import문 삭제

--- 이우빈/SimpleJWTExample/src/main/java/org/example/simplejwtexample/repository/CommentRepository.java
+++ 이우빈/SimpleJWTExample/src/main/java/org/example/simplejwtexample/repository/CommentRepository.java
@@ -1,8 +1,6 @@
 package org.example.simplejwtexample.repository;
 
 import org.example.simplejwtexample.domain.Comment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;

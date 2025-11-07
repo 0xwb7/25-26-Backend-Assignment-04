3df78ba4b112754909a31ab728f9b519ecf8af3e
0xwb7 <woobin200111@gmail.com>
2025-11-07 13:08:27 +0900

    chore: 테이블명 복수형으로 수정

--- 이우빈/SimpleJWTExample/src/main/java/org/example/simplejwtexample/domain/Comment.java
+++ 이우빈/SimpleJWTExample/src/main/java/org/example/simplejwtexample/domain/Comment.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
 public class Comment {
 
     @Id
